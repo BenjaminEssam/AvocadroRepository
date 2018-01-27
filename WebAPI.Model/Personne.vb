68bbb31a43d6595354d7e786be9ae8b03e90df94
BenjiDev <benjaminessam@gmail.com>
2018-01-27 10:23:28 -0800

    Initialisation du projet

---
+++ WebAPI.Model/Personne.vb
@@ -0,0 +1,11 @@
+﻿Public Class Personne
+    Public Property Nom As String
+    Public Property Prenom As String
+    Public Property Matricule As String
+    Public Property Adresse As String
+    Public Property Telephone As String
+    Public Property Email As String
+    Public Property Domicile As String
+    Public Property Login As String
+    Public Property MotDePasse As String
+End Class
